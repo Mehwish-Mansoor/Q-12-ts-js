3052684a3dc4ac81d5be1bd0a28c8fd03b845497
Mehwish-Mansoor <160300292+Mehwish-Mansoor@users.noreply.github.com>
2024-03-13 11:31:00 +0500

    Add files via upload

---
+++ qno12.js
@@ -0,0 +1,10 @@
+"use strict";
+// 12. Greating: Start with the array you used in Exercise 11, but instead of just
+// printing each person's name, print a message to them.The text of each message
+// should be the same, but each message should be personalized with the person's name.
+let names = ["Mehwish", "Mansoor", "fatima", "umm-e-hani"];
+let message = "How are you dear?";
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
+console.log(names[3] + " " + message);
---
+++ qno12.ts
@@ -0,0 +1,11 @@
+// 12. Greating: Start with the array you used in Exercise 11, but instead of just
+// printing each person's name, print a message to them.The text of each message
+// should be the same, but each message should be personalized with the person's name.
+
+let names:string[] =["Mehwish", "Mansoor", "fatima","umm-e-hani"]
+let message : string= "How are you dear?";
+console.log(names[0]  + " " + message)
+console.log(names[1]  + " " + message)
+console.log(names[2]  + " " + message)
+console.log(names[3]  + " " + message)
+
